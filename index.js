a1b1a337436fd8ae091e4f6a11f36b5d51971025
Abzed <ibraabzed@gmail.com>
2022-05-12 17:14:25 +0300

    commit tree cf8397d27fd2de72b91526e59462e1d64cc79b07

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return string.replace(/[^A-Za-z0-9-'\- ]+/g,'');
+  }
+
+  static titleize(string){
+    const arr = string.split(' ')
+    const prep = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let i = 0; i < arr.length; i++) {
+      if (!prep.includes(arr[i])) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+      }
+    }
+    let x = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+    let newArr = arr.slice(1).join(' ')
+    return x + ' ' + newArr
+  }
 }
+
+console.log(Formatter.titleize('the name, the name of a guy of me from.'));
